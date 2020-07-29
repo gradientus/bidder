654c87f0b817737ac2d5101da302d9ec8f98d0c9
Phil Mauracher <gradientus@gmail.com>
2020-07-28 21:33:03 -0600

    Added comments

--- bidder.js
+++ bidder.js
@@ -12,5 +12,15 @@ const randomNumbers = (bids) => {
 console.log(randomNumbers(100)); // delete this line after testing
 
 
+//find the highest bid
+//find the mean average
+//find the mode
+//find the mean
+//find the standard deviation
+//eliminate any numbers greater than 1 standard deviation
+
+//redo the mean, mode, median after the elimination
+//present on a sloppy html page
+
 
 // put in a linter
